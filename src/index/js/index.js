14c52918e7a4180cfd4da828b4c99ba2af90d0f9
HappyJeannie <436030684@qq.com>
2018-06-19 23:42:18 +0800

    打包项目

---
+++ src/index/js/index.js
@@ -0,0 +1,10 @@
+import './../css/index.less';
+
+import logo from './../img/logo.png'
+
+alert(1);
+
+let img = document.createElement('img');
+img.src = logo;
+
+document.getElementsByTagName('body')[0].appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry:'./src/index/js/index.js',
+  entry:{
+    'index':'./src/index/js/index.js'
+  },
   output:{
     filename:'bundle.js',
     path:path.resolve(__dirname,'dist')
   },
   plugins:[
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       filename:'index.html',
       template:'./src/index/index.html',
       minify:true,
-      chunks:[]  
+      chunks:['index']  
     })
-  ]
+  ],
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+      },
+      {
+        test:/\.less$/,
+        use:[
+          {
+            loader:'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }, 
+          {
+            loader: 'less-loader' 
+          }
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use:[
+          'file-loader'
+        ]
+      }
+    ]
+  }
 }
